4298d74160b221b4500c1dee04df8ba77b317270
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-04-22 17:36:45 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,28 +5,20 @@ loginFormEl.addEventListener('submit', onFormSubmit);
 function onFormSubmit(event) {
     event.preventDefault();
 
-    const {
-        elements: { email, password }
-    } = event.currentTarget;
+    const form = event.currentTarget.elements;
 
-    if (email.value === "" || password.value === "") {
+    if (form.email.value === "" || form.password.value === "") {
         return alert('Все поля должны быть заполнены')
     };
 
-    const formData = new FormData(event.currentTarget);
-
-    formData.forEach((value, name) => {
-        console.log(name, value)
-    })
-
-    //     const userEmail = event.currentTarget.elements.email.value;
-    //     const userPass = event.currentTarget.elements.password.value;
+        const userEmail = event.currentTarget.elements.email.value;
+        const userPass = event.currentTarget.elements.password.value;
  
-    //     const userData = {
-    //     userEmail,
-    //     userPass,
-    //   };
-    //     console.log(userData)
+        const userData = {
+        userEmail,
+        userPass,
+      };
+        console.log(userData)
 
     event.currentTarget.reset();
 }
